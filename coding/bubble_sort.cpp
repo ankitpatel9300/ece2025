1579c0b4c5af61b820942b37a3db2fd80fc113f3
ankitpatel9300 <ankitpatel9300@gmail.com>
2019-06-11 23:44:37 -0400

    Buble_sort program

---
+++ coding/bubble_sort.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Bubble_sort.cpp
+ *
+ *  Created on: Jun. 11, 2019
+ *  Author: Ankitkumar Patel
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number[7];
+    int i,j;
+    cout << "Please enter any 7 numbers : -"<<endl;
+    for(i=0;i<=6;i++)
+    {
+
+        cin>> number[i];
+    }
+    for (i=0;i<=5;i++)
+    {
+        for (j=i+1;j<=6;j++)
+        {
+            int temp;
+            if (number[i]<number[j])
+                {
+                    temp=number[i];
+                    number[i]= number[j];
+                    number[j]=temp;
+
+                }
+            }
+        }
+        for (i=0;i<=6;i++)
+        {
+            cout<<"=>"<<number[i]<<endl;
+
+        }
+        return 0;
+
+    }
